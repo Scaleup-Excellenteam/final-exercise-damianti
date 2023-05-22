5e1829da8da7b7e94335ec73fd655ab00b0a243f
damianti <damianti@edu.hac.ac.il>
2023-05-22 12:55:05 +0300

    pptx parsing with test

---
+++ pptx_parser/__init__.py
@@ -0,0 +1,9 @@
+import parser
+
+# Test the parse_presentation function
+if __name__ == '__main__':
+
+    parsed_data = parser.parse_presentation("asyncio-intro.pptx")
+    for i, slide in enumerate(parsed_data):
+        print(f"Slide {i + 1}:")
+        print(slide)
