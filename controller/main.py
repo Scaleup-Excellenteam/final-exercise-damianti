557b4ade3b6d53064840320a799c1b700d7d0922
damianti <damianti@edu.hac.ac.il>
2023-05-29 14:37:38 +0300

    async io with gpt works. next step: push to develop and order the code

--- controller/main.py
+++ controller/main.py
@@ -1,58 +1,71 @@
+import logging
 from pptx_parser_ import parser
-from prompt_generator import generate_prompt
+from prompt_generator.generate_prompt import PromptGenerator
 import asyncio
-import aiohttp
 import openai
 import os
+import json
 from dotenv import load_dotenv
 
+# Configure logging
+logging.basicConfig(filename='game_logs.log', level=logging.INFO, format='[%(levelname)s] %(message)s')
+# Load environment variables, including the API key
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY_3")
 
-async def send_prompt(prompt):
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY_2")
-    response = await openai.Completion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+
+async def send_prompt(prompt, slide_number):
+    openai.api_key = api_key
+    logging.info("Sending prompt...")
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.7
     )
+    logging.info(f"response from gpt: {response.choices[0].text.strip()}")
 
-    return response.choices[0].message.content
+    return f"Slide {slide_number}: {response.choices[0].text.strip()}"
 
 
 async def gpt_explainer():
 
-
     parsed_data = parser.parse_presentation(
-        "/Users/damiantissembaum/Documents/year_3/excellenteam/python/final-exercise/final-exercise-damianti/controller/pptx_parser_/asyncio-intro.pptx")
+        "/Users/damiantissembaum/Documents/year_3/excellenteam/python/final-exercise/final-exercise-damianti/controller/pptx_parser_/asyncio-intro.pptx"
+    )
+
     tasks = []
     for i, slide in enumerate(parsed_data):
-        print(f"Slide {i + 1}:")
-        print(slide)
-        # Join slide text strings with a dot separator
-        slide_text = ".".join(slide)
+        logging.info(f"Processing Slide {i + 1}")
+        logging.info("Slide content:")
+        logging.info(slide)
 
-        if slide_text:
-            # Generate prompt for the slide text
-            prompt = generate_prompt.generate_prompt(slide_text)
-            print("Prompt:", prompt)
+        generator = PromptGenerator()
 
-            task = asyncio.create_task(send_prompt(prompt))
-            tasks.append(task)
-            await asyncio.sleep(5)
-        else:
-            print("Empty slide. Skipping prompt generation.")
+        prompt = generator.generate_prompt(slide)
+
+        if not prompt:
+            logging.info(f"Slide number {i + 1} is empty.")
+
+        logging.info("Prompt:")
+        logging.info(prompt)
+
+        task = asyncio.create_task(send_prompt(prompt, i + 1))
+        tasks.append(task)
+        await asyncio.sleep(1)  # Delay between API requests
+
+
 
     # Wait for all tasks to complete
     responses = await asyncio.gather(*tasks)
 
-    # Print the responses
-    for i, response in enumerate(responses):
-        print(f"Response for Slide {i + 1}:")
-        print(response)
-        print()
+    # Save the explanations in a JSON file
+    with open("explanations.json", "w") as file:
+        json.dump(responses, file)
 
 
 if __name__ == '__main__':
-
+    logging.info("Starting GPT explainer...")
     asyncio.run(gpt_explainer())
+    logging.info("GPT explainer finished.")
--- controller/prompt_generator/generate_prompt.py
+++ controller/prompt_generator/generate_prompt.py
@@ -1,12 +1,39 @@
+# import random
+#
+#
+# def generate_prompt(slide_text):
+#     question_templates = [
+#         "Can you explain the following slide text: {}?",
+#         "What are the main points mentioned in the slide: {}?",
+#         "Please provide a summary of the content in the slide: {}.",
+#     ]
+#     prompt_template = random.choice(question_templates)
+#     prompt = prompt_template.format(slide_text)
+#     return prompt
+
+
 import random
 
 
-def generate_prompt(slide_text):
-    question_templates = [
-        "Can you explain the following slide text: {}?",
-        "What are the main points mentioned in the slide: {}?",
-        "Please provide a summary of the content in the slide: {}.",
-    ]
-    prompt_template = random.choice(question_templates)
-    prompt = prompt_template.format(slide_text)
-    return prompt
+class PromptGenerator:
+    def __init__(self):
+        self.question_templates = [
+            "Can you explain the following slide text: {}?",
+            "What are the main points mentioned in the slide: {}?",
+            "Please provide a summary of the content in the slide: {}.",
+        ]
+
+    def generate_prompt(self, slide_text):
+        slide_text = " ".join(slide_text).strip()
+        if not slide_text:
+            return ""
+
+        prompt_template = random.choice(self.question_templates)
+        prompt = prompt_template.format(slide_text)
+        return prompt
+
+if __name__ == '__main__':
+    # Usage example
+    generator = PromptGenerator()
+    prompt = generator.generate_prompt("This is the slide text.")
+    print(prompt)
